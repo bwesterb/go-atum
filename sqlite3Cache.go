c2d1eec3efdd1811fd6a986c9c06a2d49aad1cd2
Bas Westerbaan <bas@westerbaan.name>
2018-05-30 00:00:45 +0200

    Only use sqlite3 cache when cgo is available

---
+++ sqlite3Cache.go
@@ -0,0 +1,146 @@
+// +build cgo
+
+package atum
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+
+	"encoding/json"
+	"log"
+	"os"
+	"os/user"
+	"path"
+	"sync"
+	"time"
+)
+
+func init() {
+	cache = &sqlite3Cache{}
+}
+
+type sqlite3Cache struct {
+	db  *gorm.DB
+	mux sync.Mutex
+}
+
+type serverInfoRecord struct {
+	Info   []byte
+	Server string `gorm:"unique_index"`
+}
+
+type pkRecord struct {
+	Pk      []byte `gorm:"index:pk_alg_server"`
+	Server  string `gorm:"index:pk_alg_server"`
+	Alg     string `gorm:"index:pk_alg_server"`
+	Expires time.Time
+}
+
+func (cache *sqlite3Cache) ensureOpen() bool {
+	if cache.db != nil {
+		return true
+	}
+
+	usr, err := user.Current()
+	if err != nil {
+		log.Printf("atum cache: user.Current(): %v", err)
+		return false
+	}
+
+	cacheDirPath := path.Join(usr.HomeDir, ".cache", "atum")
+	if _, err = os.Stat(cacheDirPath); os.IsNotExist(err) {
+		err = os.MkdirAll(cacheDirPath, 0700)
+		if err != nil {
+			log.Printf("atum cache: os.MkdirAll(%s): %v", cacheDirPath, err)
+			return false
+		}
+	}
+
+	cachePath := path.Join(cacheDirPath, "cache.sqlite3")
+	cache.db, err = gorm.Open("sqlite3", cachePath)
+	if err != nil {
+		log.Printf("atum cache: gorm.Open(%s): %v", cachePath, err)
+		return false
+	}
+
+	if err := cache.db.AutoMigrate(&serverInfoRecord{}, &pkRecord{}).Error; err != nil {
+		log.Printf("atum cache: gorm.AutoMigrate(%s): %v", cachePath, err)
+		return false
+	}
+	return true
+}
+
+func (cache *sqlite3Cache) StorePublicKey(serverUrl string, alg SignatureAlgorithm,
+	pk []byte, expires time.Time) {
+	cache.mux.Lock()
+	defer cache.mux.Unlock()
+	if !cache.ensureOpen() {
+		return
+	}
+	if err := cache.db.Where(&pkRecord{
+		Pk:     pk,
+		Alg:    string(alg),
+		Server: serverUrl,
+	}).Assign(&pkRecord{Expires: expires}).FirstOrCreate(&pkRecord{
+		Pk:      pk,
+		Alg:     string(alg),
+		Server:  serverUrl,
+		Expires: expires,
+	}).Error; err != nil {
+		log.Printf("atum cache: StorePublicKey(): %v", err)
+	}
+}
+
+func (cache *sqlite3Cache) GetPublicKey(serverUrl string,
+	alg SignatureAlgorithm, pk []byte) *time.Time {
+	cache.mux.Lock()
+	defer cache.mux.Unlock()
+	if !cache.ensureOpen() {
+		return nil
+	}
+	var record pkRecord
+	if cache.db.Where(&pkRecord{
+		Pk:     pk,
+		Alg:    string(alg),
+		Server: serverUrl,
+	}).First(&record).RecordNotFound() {
+		return nil
+	}
+	return &record.Expires
+}
+
+func (cache *sqlite3Cache) StoreServerInfo(serverUrl string, info ServerInfo) {
+	cache.mux.Lock()
+	defer cache.mux.Unlock()
+	if !cache.ensureOpen() {
+		return
+	}
+	infoBytes, _ := json.Marshal(info)
+	if err := cache.db.Where(&serverInfoRecord{
+		Server: serverUrl,
+	}).Assign(&serverInfoRecord{
+		Info: infoBytes,
+	}).FirstOrCreate(&serverInfoRecord{
+		Info:   infoBytes,
+		Server: serverUrl,
+	}).Error; err != nil {
+		log.Printf("atum cache: StoreServerInfo(): %v", err)
+	}
+}
+
+func (cache *sqlite3Cache) GetServerInfo(serverUrl string) *ServerInfo {
+	cache.mux.Lock()
+	defer cache.mux.Unlock()
+	if !cache.ensureOpen() {
+		return nil
+	}
+	var record serverInfoRecord
+	if cache.db.Where(&serverInfoRecord{
+		Server: serverUrl,
+	}).First(&record).RecordNotFound() {
+		return nil
+	}
+	var ret ServerInfo
+	json.Unmarshal(record.Info, &ret)
+	return &ret
+}
